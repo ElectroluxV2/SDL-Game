3c8d60760eeb06394ec8352c8ec409157d50739c
ElectroluxV2 <ElectroluxV2@gmail.com>
2021-01-06 21:25:35 +0100

    FPS cap

--- SDL-Game/game.h
+++ SDL-Game/game.h
@@ -6,7 +6,7 @@ class Game {
   const int SCREEN_WIDTH = 640;
   const int SCREEN_HEIGHT = 480;
 
-  const int SCREEN_FPS = 77;
+  const int SCREEN_FPS = 60;
   const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
   bool collisionDetected = false;
